0da0c7077a10ed69efee239bef5091c0fa0562a4
wangzhong1986 <36659266@qq.com>
2016-07-20 11:08:38 +0800

    Initial Commit

---
+++ HLSPlayData/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HLSPlayData
+//
+//  Created by wangzhong on 16/7/20.
+//  Copyright © 2016年 wangzhong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HLSPlayData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HLSPlayData
+//
+//  Created by wangzhong on 16/7/20.
+//  Copyright © 2016年 wangzhong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
